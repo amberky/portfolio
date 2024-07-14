8ef053032cf38b14c78ac3ed63a0e85e86dcb6e8
kharnyeeeu <kharnyee0224@gmail.com>
2024-07-14 16:35:52 +0800

    Resized images to reduce load times

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -8,13 +8,6 @@ import { provideHttpClient } from '@angular/common/http';
 export const appConfig: ApplicationConfig = {
     providers: [
         provideRouter(routes),
-        provideHttpClient(),
-        {
-            provide: IMAGE_CONFIG,
-            useValue: {
-                disableImageSizeWarning: true, 
-                disableImageLazyLoadWarning: true
-            }
-        }
+        provideHttpClient()
     ]
 };
